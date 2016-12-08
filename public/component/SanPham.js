e7b4e99a4a11971dce3981c1a5bcf7650555b20a
Binh Nguyen <binh.nguyen@gooddata.com>
2016-12-08 23:22:34 +0700

    fix import wrong component

--- public/component/SanPham.js
+++ public/component/SanPham.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import {List, Datagrid} from 'admin-on-rest/lib/mui/list';
 import {Edit, Create} from 'admin-on-rest/lib/mui/detail';
-import {TextField, LongTextInput, ChipField} from 'admin-on-rest/lib/mui/field';
-import {DisabledInput, TextInput} from 'admin-on-rest/lib/mui/input';
+import {TextField, ChipField} from 'admin-on-rest/lib/mui/field';
+import {DisabledInput, TextInput, LongTextInput} from 'admin-on-rest/lib/mui/input';
 import {EditButton} from 'admin-on-rest/lib/mui/button';
 import {GridStyle} from '../style/default';
 
