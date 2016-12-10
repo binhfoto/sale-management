16330cd5782ff65b268ba7f568539fb341a809e8
Binh Nguyen <binh.nguyen@gooddata.com>
2016-12-10 17:44:37 +0700

    - add NhapHang table - paging, sorting - set to default font-size - refactor abstract controller

--- server/api/abstract/controller.js
+++ server/api/abstract/controller.js
@@ -2,10 +2,14 @@ var _ = require('lodash');
 var controller = {};
 
 controller.params = (Model, populate = '') => function(req, res, next, id) {
-    // use the id to find object from DB and attach to req
-    Model
-        .findById(id)
-        .populate(populate)
+    
+    var query = Model.findById(id);
+    
+    if(populate.length > 0 ) {
+        query = query.populate(populate);
+    }
+
+    query
         .exec()
         .then(
             function(item) {
@@ -16,27 +20,47 @@ controller.params = (Model, populate = '') => function(req, res, next, id) {
                     next();
                 }
             }, 
-            function(err){
+            function(err) {
                 next(err);
             }
         );
 };
 
-controller.get = (Model, populate = '') => function(req, res, next) {
-    Model
-        .find({})
-        .populate(populate)
-        .exec()
-        .then(
-            function(items){
-                //res.header("Access-Control-Expose-Headers", "x-total-count");
-                res.header("X-Total-Count", items.length);
-                res.json(items);
-            }, 
-            function(err){
-                next(err);
-            }
-        );
+controller.get = (Model, populate = '', select = '', sort = '') => function(req, res, next) {
+
+    // count all the records
+    var count = Model.count({});
+
+    // only get records between _start and _end
+    var query = Model.find();
+    
+    if(populate.length > 0 ) {
+        query = query.populate(populate);
+    }
+    if(select.length > 0) {
+        query = query.select(select);
+    }
+    if(req.query._start && req.query._end) {
+        query = query.skip(parseInt(req.query._start)).limit(parseInt(req.query._end));
+    }
+    if(req.query._sort) {
+        // id: ASC, -id: DESC
+        var sortStr = sort.concat(' ').concat(req.query._order === "ASC" ? "" : "-").concat(req.query._sort);
+        query = query.sort(sortStr);
+    }
+
+    return Promise.all([count.exec(), query.exec()]).then(
+        function(items){
+            var number = items[0];
+            var records = items[1];
+            //res.header("Access-Control-Expose-Headers", "x-total-count"); // for cors
+            res.header("X-Total-Count", Math.max(number, records.length)); // number of all records
+            res.json(records); // records on paging
+        },
+        function(err){
+            next(err);
+        }
+    );
 };
 
 controller.getOne = () => function(req, res, next) {
