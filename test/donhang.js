fcb33c17818c1856c03ed94787590b9b9f59fc8f
letrungkien0210 <letrungkien0210@gmail.com>
2017-01-05 17:24:25 +0700

    Made the UT for donhang model - Basic flow

--- test/donhang.js
+++ test/donhang.js
@@ -11,6 +11,9 @@ let DonHang = require('../server/api/donhang/model');
 let chai = require('chai');
 let chaiHttp = require('chai-http');
 let server = require('../index');
+var random = require('../server/util/random');
+const async = require('async');
+
 let should = chai.should();
 let assert = chai.assert;
 let user = {
@@ -23,6 +26,9 @@ let khachhang = {
     soDienThoai: '0987123456'
 }
 let sanphams = [];
+let sanphamDonHang = [];
+let donhang = {};
+let temp = [];
 
 let jwt = '';
 const URLs = {
@@ -37,49 +43,85 @@ chai.use(chaiHttp);
 
 describe('KhachHang', () => {
     before((done) => {
-        //create user to authentica
-        chai.request(server)
-            .post(URLs.apiUser)
-            .send(user)
-            .then((req, res) => {
-                // res.should.have.status(200);
-                jwt = req.body;
-                // done();
-            }).then((res) => {
-                //make a khachhang
-                let urlKhachHangJWT = `${URLs.apiKhachHang}?access_token=${jwt.token}`;
+        async.waterfall([
+            callback => {
+                chai.request(server)
+                    .post(URLs.apiUser)
+                    .send(user)
+                    .end((req, res) => {
+                        if (res.status != 200) {
+                            let error = {
+                                point: 'Create user',
+                                error: res.status,
+                                des: res.error
+                            };
+                            return callback(error, null);
+                        }
+                        jwt = res.body;
+                        callback(null, res.body.token);
+                    });
+            },
+            (token, callback) => {
+                let urlKhachHangJWT = `${URLs.apiKhachHang}?access_token=${token}`;
                 chai.request(server)
                     .post(urlKhachHangJWT)
                     .send(khachhang)
                     .end((req, res) => {
-                        this.khachhang = res.body;
+                        if (res.status != 200) {
+                            let error = {
+                                point: 'Create khachhang',
+                                error: res.status,
+                                des: res.error
+                            };
+                            return callback(error, null);
+                        }
+                        khachhang = res.body;
+                        callback(null, token);
                     });
-            }).then((result) => {
-                //make 2 sanphams
-                for (let i=0; i < 10; i++) {
-                    let sanphamStr = `{
-                            "maSP": "sp0${i}",
-                            "ten": "Sản phẩm ${i}",
-                            "quyCach": "Quy cach",
-                            "donVi": "Chai",
-                            "donGia": ${i * 10},
-                            "nhom": "Chuc nang"
-                        }`;
-                    let sanphamJSON = JSON.parse(sanphamStr);
-                    let urlSanPhamSWT = `${URLs.apiSanPham}?access_token=${jwt.token}`;
+            },
+            (token, callback) => {
+                makeSanphams(token, callback)
+            },
+            callback => {
+                let tongTienFloat = 0.00;
 
-                    chai.request(server)
-                        .post(urlSanPhamSWT)
-                        .send(sanphamJSON)
-                        .end((req, res) => {
-                            // this.sanphams.push(res.body);
-                            sanphams.push(res.body);
-                            if(i == 9)
-                                done();
-                        });
+                for (let i = 0; i < sanphams.length; i++) {
+                    let soLuongXuatInt = random(100, 1000);
+                    let xuatXuLyInt = random(5, 20);
+                    let chietKhauInt = random(5, 10);
+                    let thanhTienFloat = sanphams[i].donGia * soLuongXuatInt - parseFloat(((sanphams[i].donGia * soLuongXuatInt) * chietKhauInt) / 100);
+
+                    tongTienFloat += thanhTienFloat;
+
+                    sanphamDonHang.push({
+                        "maSP": sanphams[i].id,
+                        "soLuongXuat": soLuongXuatInt,
+                        "xuatXuLy": xuatXuLyInt,
+                        "chietKhau": chietKhauInt,
+                        "thanhTien": thanhTienFloat
+                    });
                 }
-            })
-            .catch(err => done(err));
+
+                callback(null);
+            },
+            callback => {
+                donhang = {
+                    'maDH': DonHang.getNextId(),
+                    'ngayTaoDH': Date.now(),
+                    'maKH': khachhang.id,
+                    'sanpham': sanphamDonHang,
+                    'thueVAT': 10,
+                    'chietKhau': 1,
+                    'thanhTien': 2
+                };
+                callback();
+            }
+        ], function(err, results) {
+            if (err) done(err);
+            done();
+        });
+
+
     });
     after((done) => {
         SanPham.remove({})
@@ -97,17 +139,149 @@ describe('KhachHang', () => {
                 (err) => {
                     throw err;
                 }
-            ).then( () => {
+            ).then(
+                () => {
+                    DonHang.remove({}).exec()
+                },
+                (err) => {
+                    throw err;
+                }).then(() => {
                 done();
             }).catch(err => done(err));
     });
 
     describe("/api/donhangs", () => {
-        it("test", (done) => {
-            done();
+        it("Make a donhang", (done) => {
+            let url = `${URLs.apiDonHang}?access_token=${jwt.token}`;
+
+            chai.request(server)
+                .post(url)
+                .send(donhang)
+                .end((req, res) => {
+                    if (res.error) {
+                        console.log(res.error);
+                        done(res.error);
+                    }
+                    donhang = res.body
+                    done();
+                });
         });
+        it("Get donhangs", (done) => {
+            let url = `${URLs.apiDonHang}?access_token=${jwt.token}`;
+
+            chai.request(server)
+                .get(url)
+                .end((req, res) => {
+                    if (res.error) {
+                        console.log(res.error);
+                        done(res.error);
+                    }
+                    done();
+                });
+        })
     });
     describe("/api/donhangs/:id", () => {
+        it("Get a donhang", (done) => {
+            let url = `${URLs.apiDonHang}/${donhang._id}?access_token=${jwt.token}`;
 
+            chai.request(server)
+                .get(url)
+                .end((req, res) => {
+                    if (res.error) {
+                        console.log(res.error);
+                        done(res.error);
+                    }
+                    done();
+                });
+        });
+        it("Edit a donhang", (done) => {
+            let url = `${URLs.apiDonHang}/${donhang._id}?access_token=${jwt.token}`;
+
+            let donhang_edited = {
+                '_id': donhang._id,
+                'maDH': DonHang.getNextId(),
+                'ngayTaoDH': Date.now(),
+                'maKH': khachhang.id,
+                'sanpham': sanphamDonHang,
+                'thueVAT': 20,
+                'chietKhau': 1,
+                'thanhTien': 2
+            };
+
+            chai.request(server)
+                .put(url)
+                .send(donhang_edited)
+                .end((req, res) => {
+                    if (res.error) {
+                        console.log(res.error);
+                        done(res.error);
+                    }
+                    // console.log(res.body);
+                    done();
+                });
+        });
+        it("Delete a donhang", (done) => {
+            let url = `${URLs.apiDonHang}/${donhang._id}?access_token=${jwt.token}`;
+
+            chai.request(server)
+                .del(url)
+                .end((req, res) => {
+                    if (res.error) {
+                        console.log(res.error);
+                        done(res.error);
+                    }
+                    done();
+                });
+        });
     });
 });
+
+function makeSanphams(token, callback) {
+    let urlSanPhamSWT = `${URLs.apiSanPham}?access_token=${token}`;
+    let sanphamStr = `{
+                            "maSP": "sp01",
+                            "ten": "Sản phẩm 1",
+                            "quyCach": "Quy cach",
+                            "donVi": "Chai",
+                            "donGia": 10,
+                            "nhom": "Chuc nang"
+                        }`;
+    let sanphamArr = [];
+
+    for (let i = 0; i < 10; i++) {
+        let sanpham = {
+            'maSP': `sp${i}`,
+            'ten': `Sản phầm ${i}`,
+            'quyCach': `Quy cách ${i}`,
+            'donVi': `Chai`,
+            'donGia': i * 10,
+            'nhom': 'Chuc nang'
+        };
+        sanphamArr.push(sanpham);
+    }
+
+    async.each(sanphamArr, (sanpham, callback) => {
+        chai.request(server)
+            .post(urlSanPhamSWT)
+            .send(sanpham)
+            .end((req, res) => {
+                if (res.status != 200) {
+                    let error = {
+                        point: 'Make a sanpham',
+                        error: res.status,
+                        des: res.error
+                    };
+                    return callback(error, null);
+                }
+                sanphams.push(res.body);
+                callback();
+            });
+    }, err => {
+        if (err)
+            console.log(err);
+        else {
+            console.log("Imported 10 sanphams");
+        }
+        callback(null);
+    });
+}
